892d391f14764cb9448dd854ef79bbb8f300714f
Morgan Harrison <mrgnharrisn@gmail.com>
2024-03-28 01:13:56 +1300

    Added LSP and Formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,8 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "rust", "cpp", "c", "zig"},
+            auto_install = true,
+            -- ensure_installed = {"lua", "rust", "cpp", "c", "zig"},
             highlight = { enable = true },
             indent = { enable = true }
         })
